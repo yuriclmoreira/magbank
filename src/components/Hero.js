6d65b4ad71ebe3ab0e7b4340cb33e4c26427a1cb
Yuri Moreira <yuriclmoreira@terra.com.br>
2022-10-26 17:30:25 -0300

    (homepage):create homepage page later hero,create institutional page and cards info

---
+++ src/components/Hero.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, Col, Card, Button, Image } from "react-bootstrap";
+import logo from "../assets/logo__magic-pay--mobile.png";
+import logoDesktop from "../assets/logo__magic-pay.png";
+import "./Hero.scss";
+
+const Hero = () => (
+  <Card className="text-center text-light hero">
+    <Row className="my-lg-5">
+      <Col lg className="text-lg-end mt-5">
+        <Image className="d-lg-none" src={logo} />
+        <Image className="d-none d-lg-inline-block" src={logoDesktop} />
+      </Col>
+      <Col lg className="text-lg-start my-5">
+        <p>Pague suas contas pelo nosso APP</p>
+        <Button variant="outline-light">Abra a sua conta</Button>
+      </Col>
+    </Row>
+  </Card>
+);
+
+export default Hero;
